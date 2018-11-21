9ce0e5696c99265e91eac7d822841bd0b35e514e
Avyaan <avi@Avyaans-MacBook-Pro.local>
2018-11-21 04:46:08 +0000

    a

---
+++ firstapp/src/index.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    return(
+        <div>
+            <h1>This is my react app</h1>
+            <h2>This is first app</h2>
+        </div>
+    )
+}
+
+ReactDOM.render(<App/>, document.getElementById("root"))
+
+
+
+
+
+
+
+
+
+
+/*
+functional (stateless)
+-Light weight but less function
+class based(statefull)
+-Heavy in weighr with more function
+
+
+function add(a,b){
+    return a+b
+}
+const add = (a,b) => {
+    return a+b
+}
+
+JSX
+
+*/
