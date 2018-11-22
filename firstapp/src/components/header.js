25d61be8371c02977a8ac3aba1ff40fe90cbb537
aakashDeveloper <ahanda205@gmail.com>
2018-11-22 03:53:32 +0000

    a

---
+++ firstapp/src/components/header.js
@@ -0,0 +1,74 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            title:'React App',
+            keyword:'user search'
+        }
+    }
+
+    inputChange(event){
+        console.log(event.target.value)
+        this.setState({keyword:event.target.value})
+    }
+
+    render(){
+        return(
+                <header>
+                    <div className="logo"
+                    onClick = {() => { console.log("clicked")}}>
+                    {this.state.title}
+                    </div>
+                    <center>
+                        <input onChange={this.inputChange.bind(this)}/>
+                        <h2>{this.state.keyword}</h2>
+                    </center>  
+                </header>
+        )
+    }
+}
+
+
+export default Header;
+
+
+
+
+
+/*
+
+state  = local variable
+
+props = use to share the data
+
+
+A          Props             B
+state    =========>          state
+
+const Header = () => {
+    return(
+        <div>
+            <h1>Logo</h1>
+        </div>
+    )
+}
+
+const styles = {
+            header:{
+                background: 'orange'
+            },
+            logo:{
+                color: '#fff',
+                textAlign:'center',
+                fontSize:'30px'
+            }
+        }
+
+<header style={styles.header}>
+                    <div style={styles.logo}>Logo</div>
+                    <input />
+                </header>
+*/
--- firstapp/src/index.js
+++ firstapp/src/index.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom';
 
+import Header from './components/header';
+
 const App = () => {
     return(
         <div>
+            <Header></Header>
             <h1>This is my react app</h1>
             <h2>This is first app</h2>
         </div>
